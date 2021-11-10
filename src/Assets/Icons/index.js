19666e3508ca4c459e682bfccb4120fac5a6655c
genilson fernandes <geniilsonfernandes@gmail.com>
2021-11-10 10:30:30 -0300

    gamesSlider feito

--- src/Assets/Icons/index.js
+++ src/Assets/Icons/index.js
@@ -1,5 +1,6 @@
 export { ReactComponent as Logo } from "./logo.svg";
 export { ReactComponent as SearchIcon } from "./search.svg";
-export { ReactComponent as LoadingIcon } from "./loading.svg"; 
-export { ReactComponent as ArrowDonwIcon } from "./arrow__donw.svg"; 
-export { ReactComponent as MenuDotsIcon } from "./menu__dots.svg"; 
+export { ReactComponent as LoadingIcon } from "./loading.svg";
+export { ReactComponent as ArrowDonwIcon } from "./arrow__donw.svg";
+export { ReactComponent as MenuDotsIcon } from "./menu__dots.svg";
+export { ReactComponent as ArrowIcon } from "./arrow.svg";
--- src/pages/Home/GamesSlider/GamesSlider.jsx
+++ src/pages/Home/GamesSlider/GamesSlider.jsx
@@ -1,56 +1,91 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import GameCard from "../../../components/GameCard/GameCard";
 import Container from "../../../components/Layout/Container";
-
+import styles from "./GamesSlider.module.scss";
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Pagination } from "swiper";
 import "swiper/swiper-bundle.css";
+import { ArrowIcon } from "../../../assets/Icons";
 SwiperCore.use([Navigation, Pagination]);
 
 const games = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const slideResposiveConfig = {
+  // when window width is >= 640px
+  375: {
+    spaceBetween: 10,
+    slidesPerView: 2,
+  },
+  640: {
+    spaceBetween: 16,
+    slidesPerView: 2,
+  },
+  700: {
+    spaceBetween: 16,
+    slidesPerView: 3,
+  },
+  768: {
+    slidesPerView: 5,
+  },
+};
 
 function GamesSlider() {
-  const slideResposiveConfig = {
-    // when window width is >= 640px
-    375: {
-      spaceBetween: 10,
-      slidesPerView: 2,
-    },
-    640: {
-      spaceBetween: 16,
-      slidesPerView: 2,
-    },
-    700: {
-      spaceBetween: 16,
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 5,
-    },
-  };
+  const [next, setNext] = useState(null);
+  const [prev, setPrev] = useState(null);
+  const prevEl = useRef(null);
+  const nextEl = useRef(null);
+
+  useEffect(() => {
+    setNext(nextEl.current);
+    setPrev(prevEl.current);
+  }, [nextEl, prev]);
 
   return (
     <Container>
-      <Swiper
-        breakpoints={slideResposiveConfig}
-        slidesPerView={2}
-        spaceBetween={16}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-        wrapperTag="div"
-        navigation={{
-          prevEl: null,
-          nextEl: null,
-        }}
-      >
-        {games.map((id) => (
-          <SwiperSlide>
-            <GameCard key={id} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <GameCard />
+      <div className={styles.slider}>
+        <div className={styles.head}>
+          <div className={styles.left}>
+            <h2 className={styles.title}>
+              <span>Now</span> and Trending
+            </h2>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.control}>
+              <button
+                className={styles.control__button}
+                name="prev"
+                ref={prevEl}
+              >
+                <ArrowIcon />
+              </button>
+              <button
+                className={styles.control__button}
+                name="next"
+                ref={nextEl}
+              >
+                <ArrowIcon />
+              </button>
+            </div>
+          </div>
+        </div>
+        <Swiper
+          breakpoints={slideResposiveConfig}
+          slidesPerView={2}
+          spaceBetween={16}
+          onSwiper={(swiper) => console.log(swiper)}
+          wrapperTag="div"
+          navigation={{
+            prevEl: prev,
+            nextEl: next,
+          }}
+        >
+          {games.map((id) => (
+            <SwiperSlide>
+              <GameCard key={id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </Container>
   );
 }

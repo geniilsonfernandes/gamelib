599a5e6d8fd637c675e6e07adf0a4d8df618432e
genilson fernandes <geniilsonfernandes@gmail.com>
2021-11-12 13:19:13 -0300

    ajuste erro deploy - colocando de volta os icones

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "../Layout/Container";
 import styles from "./Footer.module.scss";
 import Subscribe from "./Subscribe/Subscribe";
-
+import { PhoneIcon, MailIcon } from "../../Icons/Icon";
 function Footer() {
   return (
     <footer className={styles.s__footer}>
@@ -20,12 +20,12 @@ function Footer() {
             <ul>
               <li>
                 <a href="mailto:geilsonfernandes">
-                  geniilsonfernandes@gmail.com
+                  <MailIcon /> geniilsonfernandes@gmail.com
                 </a>
               </li>
               <li>
                 <a href="https://api.whatsapp.com/send?phone=5522996021627">
-                  (22) 9 9602-1627
+                  <PhoneIcon /> (22) 9 9602-1627
                 </a>
               </li>
             </ul>

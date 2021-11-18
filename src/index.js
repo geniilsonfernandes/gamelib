351b4047d2bf593a4600345c33f3e16446a6a953
Genilson Fernandes <geniilsonfernandes@gmail.com>
2021-11-18 09:38:24 -0300

    tentantiva 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./Hooks/user/index";
-import { SearchProvider } from "./Hooks/search/index";
+import { UserProvider } from "./Hooks/user/useProvider";
+import { SearchProvider } from "./Hooks/search/SearchProvider";
 import App from "./App";
 
 ReactDOM.render(

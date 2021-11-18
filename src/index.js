d24ebb6247a3b400285276d2e0d129e30f3d9c95
Genilson Fernandes <geniilsonfernandes@gmail.com>
2021-11-18 09:28:32 -0300

    deploy da aplicação

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./Hooks/User";
-import { SearchProvider } from "./Hooks/Search";
+import { UserProvider } from "./Hooks/User.js";
+import { SearchProvider } from "./Hooks/Search.js";
 import App from "./App";
 
 ReactDOM.render(
